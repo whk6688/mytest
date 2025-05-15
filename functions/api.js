82143cbc4ee869f135a942c72e9ad612aa261aa6
wanghaikuan <wanghaikuan@nanshe-arch.com>
2025-05-15 21:17:25 +0800

    Revert "调整"
    This reverts commit 3c7c85883bd483f9c45ed3b5cae5582ed5fa5c3d.
    
    Revert "调整"
    
    This reverts commit 030d340cf636ae420c235a354466fc11ab62f8d3.
    
    Revert "调整"
    
    This reverts commit e46e3dedfd10fc0e05e5cf7e3f51fc7f46367555.

--- functions/api.js
+++ functions/api.js
@@ -1,27 +1,31 @@
 export async function onRequestPost({ request }) {
   const { text, pms } = await request.json();
   const body = {
-	"system_instruction":
-            {"parts": {
-                "text": `现在你是塔罗牌大师，根据我所选的牌去根据问题去解析，使用的是22张大阿尔克那牌，{"0": "愚者","1": "魔术师","2": "女祭司","3": "皇后","4": "皇帝","5": "教皇","6": "恋人","7": "战车","8": "力量","9": "隐士","10": "命运之轮","11": "正义","12": "倒吊人","13": "死神","14": "节制","15": "恶魔","16": "塔","17": "星星","18": "月亮","19": "太阳","20": "审判","21": "世界"}，下面我将以数组的形式给你卡牌，其中isReversed代表是否为逆位，no为从 0 到 21 对应的22张大阿尔克那牌，你在解析的时候，需要把0-21用22张大阿尔克那牌对应的名称回答，你只需要解释卡牌的含义及解析，最后结尾用百分比表示问题的概率，不用回答多余的话`}},
-    "contents": [
-            {
-                "role": "user",
-                "parts":[
-                    {
-                     "text": `卡牌数组是：${JSON.stringify(pms)}，问题是：'${text}？'，请帮我解析`
-                    }
-                ]
-            },
-
-        ]	
-	
+    "messages": [
+      {
+        "role": "system",
+        "content": `现在你是塔罗牌大师，根据我所选的牌去根据问题去解析，使用的是22张大阿尔克那牌，{"0": "愚者","1": "魔术师","2": "女祭司","3": "皇后","4": "皇帝","5": "教皇","6": "恋人","7": "战车","8": "力量","9": "隐士","10": "命运之轮","11": "正义","12": "倒吊人","13": "死神","14": "节制","15": "恶魔","16": "塔","17": "星星","18": "月亮","19": "太阳","20": "审判","21": "世界"}，下面我将以数组的形式给你卡牌，其中isReversed代表是否为逆位，no为从 0 到 21 对应的22张大阿尔克那牌，你在解析的时候，需要把0-21用22张大阿尔克那牌对应的名称回答，你只需要解释卡牌的含义及解析，最后结尾用百分比表示问题的概率，不用回答多余的话`
+      },
+      {
+        "role": "user",
+        "content": `卡牌数组是：${JSON.stringify(pms)}，问题是：'${text}？'，请帮我解析`
+      }
+    ],
+    "stream": false,
+    "model": "glm-4-flash",
+    "temperature": 0,
+    "presence_penalty": 0,
+    "frequency_penalty": 0,
+    "top_p": 1
   }
-  const res = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAY9YffF1UY4k48_Fs6sbCm7BYJPG25OSE", {
+  const res = await fetch("https://nas-ai.4ce.cn/v1/chat/completions", {
+    "headers": {
+      "authorization": "Bearer sk-L8W2WtnCtdwG6nctF975D0E770144dE5Be3123Fa16720a03",
+      "content-type": "application/json"
+    },
     "body": JSON.stringify(body),
     "method": "POST"
   });
   const data = await res.json();
-  alert(data)
-  return new Response("我不知道");
+  return new Response(data.choices[0].message.content);
 }
